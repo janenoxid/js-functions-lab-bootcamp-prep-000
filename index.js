f1e3fe19d2fbd48d77e591586ba144e80fb4bec2
Desiree Jane Dixon <desireejanedixon@gmail.com>
2018-02-13 04:08:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function happyHolidaysto(name){
 function happyHolidayto(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown(){
-  
+function holidayCountdown(holiday, days){
+  return `It's ${days} days until ${holiday}!`
 }
